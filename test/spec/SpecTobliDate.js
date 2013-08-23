32d53dc151ff6e793d3c8dd679ecc3c33456172d
Loren Osborn <linux_dr@yahoo.com>
2013-08-22 22:48:24 -0700

    Added test for FIXME_formatUtcDateWithLocalTimeWithSeconds()

--- test/spec/SpecTobliDate.js
+++ test/spec/SpecTobliDate.js
@@ -55,6 +55,84 @@ describe("getTobliDateConstructor", function() {
 	    	new (getTobliDateConstructor())(1,2,3,4,5,6,7,8);
 	    }).toThrow('Too many variadic constructor arguments without eval support (disabled in Chrome plugins)');
 	});
+
+	var testZeroPadding = (function testZeroPadding(setter, test) {
+		setter(0);
+		test('00');
+		setter(1);
+		test('01');
+		setter(9);
+		test('09');
+		setter(10);
+		test('10');
+		setter(12);
+		test('12');
+	});
+
+	var DummyDate = (function DummyDate() {
+		return {
+			getUTCFullYear: (function getUTCFullYear() {return DummyDate.utcYear;}),
+			getUTCMonth: (function getUTCMonth() {return DummyDate.utcMonth;}),
+			getUTCDate: (function getUTCDate() {return DummyDate.utcDay;}),
+			getFullYear: (function getFullYear() {return DummyDate.year;}),
+			getMonth: (function getMonth() {return DummyDate.month;}),
+			getDate: (function getDate() {return DummyDate.day;}),
+			getHours: (function getHours() {return DummyDate.hours;}),
+			getMinutes: (function getMinutes() {return DummyDate.minutes;}),
+			getSeconds: (function getSeconds() {return DummyDate.seconds;}),
+		};
+	});
+	DummyDate.utcYear = 2000;
+	DummyDate.utcMonth = 0;
+	DummyDate.utcDay = 0;
+	DummyDate.year= 2000;
+	DummyDate.month = 0;
+	DummyDate.day = 0;
+	DummyDate.hours = 0;
+	DummyDate.minutes = 0;
+	DummyDate.seconds = 0;
+	
+	it("returns a constructor with working FIXME_formatUtcDateWithLocalTimeWithSeconds() method", function() {
+		var DummyTobliDate = getTobliDateConstructor(DummyDate);
+		var testYear = (function testYear(year) {
+			year = '' + year;
+			DummyDate.utcYear = year;
+			testZeroPadding(
+				(function (month) { DummyDate.utcMonth = month - 1; }),
+				(function (month) {
+					if (month != '00') { // invalid test cases
+						testZeroPadding(
+							(function (day) { DummyDate.utcDay = day; }),
+							(function (day) {
+								if (day != '00') { // invalid test cases
+									testZeroPadding(
+										(function (hour) { DummyDate.hours = hour; }),
+										(function (hour) {
+											testZeroPadding(
+												(function (min) { DummyDate.minutes = min; }),
+												(function (min) {
+													testZeroPadding(
+														(function (sec) { DummyDate.seconds = sec; }),
+														(function (sec) {
+															var dateObj = new DummyTobliDate();
+															expect(dateObj.FIXME_formatUtcDateWithLocalTimeWithSeconds()).
+															    toEqual(year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec);
+										                })
+													);
+								                })
+											);
+						                })
+									);
+								}
+			                })
+						);
+					}
+                })
+			);
+		});
+		testYear(2000);
+		testYear(2005);
+	});
 	
 	it("returns a constructor with working isSameDate() method", function() {
 		var sampleYears = [2000,2001,2002];
@@ -69,6 +147,7 @@ describe("getTobliDateConstructor", function() {
 		var lateControlDate = new TobliDate(controlDateString + ' 23:59:59');
 		var expectedMatch;
 		var matchCount = 0;
+		var testCount = 0;
 		for (i = 0; i < sampleMonths.length; i++) {
 			for (j = 0; j < 3; j++) {
 				for (k = 0; k < sampleYears.length; k++) {
@@ -76,9 +155,11 @@ describe("getTobliDateConstructor", function() {
 					if (expectedMatch) {
 						matchCount++;
 						expect(expectedMatch).toEqual(true);
+						expect(testCount).toEqual((3*3) + 3 + 1);
 					} else {
 						expect(expectedMatch).toEqual(false);
 					}
+					testCount++;
 					comparisonDateString = sampleMonths[i] + ' ' + (j + FIRST_VALID_DAY_OF_MONTH) + ', ' + sampleYears[k];
 					earlyComparisonDate = new TobliDate(comparisonDateString + ' 00:00:00');
 					lateComparisonDate = new TobliDate(comparisonDateString + ' 23:59:59');
@@ -89,6 +170,7 @@ describe("getTobliDateConstructor", function() {
 				}
 			}
 		}
+		expect(testCount).toEqual(3*3*3);
 		expect(matchCount).toEqual(1);
 	});
 	
