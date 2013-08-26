ffe9d06d9f8bd7adbabf2c4ef902e8cfc3333a4a
Loren Osborn <linux_dr@yahoo.com>
2013-08-25 18:05:31 -0700

    Finished adding tests for existing TobliDate methods
    Cleaned up jasmine include directory

--- test/spec/SpecTobliDate.js
+++ test/spec/SpecTobliDate.js
@@ -4,6 +4,45 @@ describe("getTobliDateConstructor", function() {
 		return new Date(initalDateSpec);
 	});
 	OneArgDateWithFakeCurrentTime.fakeCurrentTimeString = '2013-09-10 08:11:07';
+
+	var testZeroPadding = (function testZeroPadding(setter, test) {
+		setter(0);
+		test('00');
+		setter(1);
+		test('01');
+		setter(9);
+		test('09');
+		setter(10);
+		test('10');
+		setter(12);
+		test('12');
+	});
+
+	var DummyDate = (function DummyDate(altSourceObj) {
+		if (altSourceObj == null) {
+			altSourceObj = DummyDate;
+		}
+		return {
+			getUTCFullYear: (function getUTCFullYear() {return altSourceObj.utcYear;}),
+			getUTCMonth: (function getUTCMonth() {return altSourceObj.utcMonth;}),
+			getUTCDate: (function getUTCDate() {return altSourceObj.utcDay;}),
+			getFullYear: (function getFullYear() {return altSourceObj.year;}),
+			getMonth: (function getMonth() {return altSourceObj.month;}),
+			getDate: (function getDate() {return altSourceObj.day;}),
+			getHours: (function getHours() {return altSourceObj.hours;}),
+			getMinutes: (function getMinutes() {return altSourceObj.minutes;}),
+			getSeconds: (function getSeconds() {return altSourceObj.seconds;}),
+		};
+	});
+	DummyDate.utcYear = 2000;
+	DummyDate.utcMonth = 0;
+	DummyDate.utcDay = 0;
+	DummyDate.year = 2000;
+	DummyDate.month = 0;
+	DummyDate.day = 0;
+	DummyDate.hours = 0;
+	DummyDate.minutes = 0;
+	DummyDate.seconds = 0;
 	
 	it("should be a function that returns a well behaved constructor", function() {
 		expect(getTobliDateConstructor).isAFunction({withName:'getTobliDateConstructor'});
@@ -12,6 +51,7 @@ describe("getTobliDateConstructor", function() {
 	
 	it("returns a constructor with expected methods", function() {
 		var expectedMethods = [
+            // 'formatTimeAndDate',
 		    'formatDateAndTimeWithLabeledTodayDate',
 		    'FIXME_formatDayMonthAndTimeWithImplicitTodayDate',
 		    'FIXME_formatUtcDateWithLocalTimeWithSeconds',
@@ -55,48 +95,149 @@ describe("getTobliDateConstructor", function() {
 	    	new (getTobliDateConstructor())(1,2,3,4,5,6,7,8);
 	    }).toThrow('Too many variadic constructor arguments without eval support (disabled in Chrome plugins)');
 	});
-
-	var testZeroPadding = (function testZeroPadding(setter, test) {
-		setter(0);
-		test('00');
-		setter(1);
-		test('01');
-		setter(9);
-		test('09');
-		setter(10);
-		test('10');
-		setter(12);
-		test('12');
+	
+	/*
+	it("returns a constructor with working formatTimeAndDate() method (when not commented out)", function() {
+		var DummyTobliDate = getTobliDateConstructor(DummyDate);
+		var testCount = 0;
+		var testYear = (function testYear(year) {
+			DummyDate.year = year;
+			year = '' + year;
+			testZeroPadding(
+				(function (month) { DummyDate.month = month - 1; }),
+				(function (month) {
+					if (month != '00') { // invalid test cases
+						testZeroPadding(
+							(function (day) { DummyDate.day = day; }),
+							(function (day) {
+								if (day != '00') { // invalid test cases
+									testZeroPadding(
+										(function (hour) { DummyDate.hours = hour; }),
+										(function (hour) {
+											testZeroPadding(
+												(function (min) { DummyDate.minutes = min; }),
+												(function (min) {
+													var dateObj = new DummyTobliDate();
+													expect(dateObj.formatTimeAndDate()).
+													    toEqual(year + '-' + month + '-' + day + ' ' + hour + ':' + min);
+													testCount++;
+								                })
+											);
+						                })
+									);
+								}
+			                })
+						);
+					}
+                })
+			);
+		});
+		testYear(2000);
+		testYear(2005);
+		expect(testCount).toEqual(2*4*4*5*5);
 	});
-
-	var DummyDate = (function DummyDate() {
-		return {
-			getUTCFullYear: (function getUTCFullYear() {return DummyDate.utcYear;}),
-			getUTCMonth: (function getUTCMonth() {return DummyDate.utcMonth;}),
-			getUTCDate: (function getUTCDate() {return DummyDate.utcDay;}),
-			getFullYear: (function getFullYear() {return DummyDate.year;}),
-			getMonth: (function getMonth() {return DummyDate.month;}),
-			getDate: (function getDate() {return DummyDate.day;}),
-			getHours: (function getHours() {return DummyDate.hours;}),
-			getMinutes: (function getMinutes() {return DummyDate.minutes;}),
-			getSeconds: (function getSeconds() {return DummyDate.seconds;}),
-		};
+	*/
+	
+	it("returns a constructor with working formatDateAndTimeWithLabeledTodayDate() method", function() {
+		var DummyTobliDate = getTobliDateConstructor(DummyDate);
+		var testCount = 0;
+		var testYear = (function testYear(year) {
+			DummyDate.year = year;
+			year = '' + year;
+			testZeroPadding(
+				(function (month) { DummyDate.month = month - 1; }),
+				(function (month) {
+					if (month != '00') { // invalid test cases
+						testZeroPadding(
+							(function (day) { DummyDate.day = day; }),
+							(function (day) {
+								if (day != '00') { // invalid test cases
+									testZeroPadding(
+										(function (hour) { DummyDate.hours = hour; }),
+										(function (hour) {
+											testZeroPadding(
+												(function (min) { DummyDate.minutes = min; }),
+												(function (min) {
+													expect(DummyDate.year + '').toEqual(year);
+													var dateObj = new DummyTobliDate();
+													expect(dateObj.formatDateAndTimeWithLabeledTodayDate()).
+													    toEqual('Today ' + hour + ':' + min);
+													dateObj = new DummyTobliDate({
+														year: DummyDate.year,
+														month: DummyDate.month,
+														day: DummyDate.day,
+														hours: DummyDate.hours,
+														minutes: DummyDate.minutes
+													});
+													DummyDate.year++;
+													expect(dateObj.formatDateAndTimeWithLabeledTodayDate()).
+													    toEqual(year + '-' + month + '-' + day + ' ' + hour + ':' + min);
+													testCount++;
+													expect(DummyDate.year + '').not.toEqual(year);
+													DummyDate.year--;
+													expect(DummyDate.year + '').toEqual(year);
+								                })
+											);
+						                })
+									);
+								}
+			                })
+						);
+					}
+                })
+			);
+		});
+		testYear(2000);
+		testYear(2005);
+		expect(testCount).toEqual(2*4*4*5*5);
 	});
-	DummyDate.utcYear = 2000;
-	DummyDate.utcMonth = 0;
-	DummyDate.utcDay = 0;
-	DummyDate.year= 2000;
-	DummyDate.month = 0;
-	DummyDate.day = 0;
-	DummyDate.hours = 0;
-	DummyDate.minutes = 0;
-	DummyDate.seconds = 0;
+	
+	it("returns a constructor with working FIXME_formatDayMonthAndTimeWithImplicitTodayDate() method", function() {
+		var DummyTobliDate = getTobliDateConstructor(DummyDate);
+		var testCount = 0;
+		var testDayMonth = (function testDayMonth(day, month) {
+			DummyDate.day = day;
+			day = '' + day;
+			DummyDate.month = month - 1;
+			month = '' + month;
+			testZeroPadding(
+				(function (hour) { DummyDate.hours = hour; }),
+				(function (hour) {
+					testZeroPadding(
+						(function (min) { DummyDate.minutes = min; }),
+						(function (min) {
+							var dateObj = new DummyTobliDate();
+							expect(dateObj.FIXME_formatDayMonthAndTimeWithImplicitTodayDate()).
+							    toEqual(hour + ':' + min);
+							dateObj = new DummyTobliDate({
+								year: DummyDate.year,
+								month: DummyDate.month,
+								day: DummyDate.day,
+								hours: DummyDate.hours,
+								minutes: DummyDate.minutes
+							});
+							DummyDate.year++;
+							expect(dateObj.FIXME_formatDayMonthAndTimeWithImplicitTodayDate()).
+							    toEqual(day + '/' + month + ' ' + hour + ':' + min);
+							testCount++;
+		                })
+					);
+                })
+			);
+		});
+		testDayMonth(10,11);
+		testDayMonth(12,3);
+		testDayMonth(4,15);
+		testDayMonth(6,7);
+		expect(testCount).toEqual(4*5*5);
+	});
 	
 	it("returns a constructor with working FIXME_formatUtcDateWithLocalTimeWithSeconds() method", function() {
 		var DummyTobliDate = getTobliDateConstructor(DummyDate);
+		var testCount = 0;
 		var testYear = (function testYear(year) {
-			year = '' + year;
 			DummyDate.utcYear = year;
+			year = '' + year;
 			testZeroPadding(
 				(function (month) { DummyDate.utcMonth = month - 1; }),
 				(function (month) {
@@ -117,6 +258,7 @@ describe("getTobliDateConstructor", function() {
 															var dateObj = new DummyTobliDate();
 															expect(dateObj.FIXME_formatUtcDateWithLocalTimeWithSeconds()).
 															    toEqual(year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec);
+															testCount++;
 										                })
 													);
 								                })
@@ -132,6 +274,7 @@ describe("getTobliDateConstructor", function() {
 		});
 		testYear(2000);
 		testYear(2005);
+		expect(testCount).toEqual(2*4*4*5*5*5);
 	});
 	
 	it("returns a constructor with working isSameDate() method", function() {
