85eba6dac6a33f667cb61a1fdf172d0078dcbc10
Loren Osborn <linux_dr@yahoo.com>
2013-10-15 23:39:45 -0700

    WIP: Fixing well behaved constructor matcher messages

--- test/customMatchers.js
+++ test/customMatchers.js
@@ -92,28 +92,46 @@
         if (parameterObject.withArbitrary == 'parentClass') {
             cleanParamObj.withParentClass = null;
         }
-        var messages = [];
+        var messageEnding = '';
+        var actualStr = jasmine.pp(this.actual);
+        var actualSuffix = '';
+        var affirmation = ' ';
+        var affirmationSuffix = '.';
+        var negation = ' not ';
+        var negationSuffix = '.';
+        var printableArgsList = [];
         var retVal = isAFunctionMatcher.apply(this, cleanParamObj.isFunctionParams);
         var constructorWrapper = null;
         var testObj = null;
+        var callableName;
+        var specParts = []
+        var i;
+        if (isAFunctionMatcher.call(this)) {
+            actualStr = jasmine.getNameOfFunction(this.actual);
+            if (actualStr == null) {
+                actualStr = 'anonymous function';
+            }
+        }
         if (!retVal) {
-            if (this.message) {
-                messages.push((this.message())[0]);
-            } else {
-                messages.push("Expected " + jasmine.pp(this.actual) + " to be a function.");
+            messageEnding = 'to be a function';
+            if (cleanParamObj.isFunctionParams.length == 1) {
+                messageEnding += ' with name ' + cleanParamObj.isFunctionParams[0].withName;
             }
         } else {
             retVal = (this.actual.prototype !== jasmine.undefined) && (this.actual.prototype !== null);
             if (!retVal) {
-                messages.push("Expected " + jasmine.pp(this.actual) + " to have a non-null prototype.");
+                messageEnding = 'to have a non-null prototype';
             } else {
                 retVal = (this.actual.prototype.constructor === this.actual);
                 if (!retVal) {
-                    messages.push("Expected " + jasmine.pp(this.actual) + "'s prototype to point to itself.");
+                    actualSuffix = "'s prototype";
+                    messageEnding = 'to point to itself';
                 } else {
                     retVal = (cleanParamObj.withParentClass === null) || (Object.getPrototypeOf(this.actual.prototype) === cleanParamObj.withParentClass.prototype);
                     if (!retVal) {
-                        messages.push("Expected parent class " + getNameOfFunction(cleanParamObj.withParentClass) + " instead of " + getNameOfFunction(Object.getPrototypeOf(this.actual.prototype).constructor) + ".");
+                        actualSuffix = "'s parent class";
+                        messageEnding = 'to be ' + getNameOfFunction(cleanParamObj.withParentClass);
+                        affirmationSuffix = " instead of " + getNameOfFunction(Object.getPrototypeOf(this.actual.prototype).constructor) + ".";
                     } else {
                         constructorWrapper = function (constructor) {
                             return constructor.apply(this, cleanParamObj.whenCalledWith);
@@ -126,17 +144,38 @@
                             (testObj.constructor === cleanParamObj.returningObjectOfClass) &&
                             (Object.getPrototypeOf(testObj) === cleanParamObj.returningObjectOfClass.prototype);
                         if (!retVal) {
-                            messages.push("Constructor doesn't return object of requested type.");
+                            messageEnding = 'to create objects of class ' + getNameOfFunction(cleanParamObj.returningObjectOfClass);
+                            affirmationSuffix = " instead of objects of class " + getNameOfFunction(testObj.constructor) + ".";
+                        } else {
+                            messageEnding = 'to be a well behaved constructor';
+                            if (cleanParamObj.isFunctionParams.length == 1) {
+                                specParts.push('name ' + cleanParamObj.isFunctionParams[0].withName);
+                            }
+                            if (cleanParamObj.withParentClass) {
+                                specParts.push('parent class ' + getNameOfFunction(cleanParamObj.withParentClass));
+                            }
+                            if (specParts.length > 0) {
+                                messageEnding += ' with ' + specParts.join(' and ');
+                            }
+                            for (i in cleanParamObj.whenCalledWith) {
+                                printableArgsList[i] = jasmine.pp(cleanParamObj.whenCalledWith[i]);
+                            }
+                            callableName = jasmine.getNameOfFunction(this.actual) || ('(' + this.actual.toString() + ')');
+                            messageEnding += (
+                                ', creating objects of class ' + getNameOfFunction(cleanParamObj.returningObjectOfClass) +
+                                ' when called "new ' + callableName + '(' + printableArgsList.join(', ') + ')"'
+                            );
                         }
                     }
                 }
             }
         }
-        if (!retVal) {
-            this.message = function() {
-                return messages;
-            };
-        }
+        this.message = (function() {
+            return [
+                ('Expected ' + actualStr + actualSuffix + affirmation + messageEnding + affirmationSuffix),
+                ('Expected ' + actualStr + actualSuffix + negation + messageEnding + negationSuffix)
+            ];
+        });
         return retVal;
     };
     beforeEach(function() {
@@ -263,6 +302,7 @@
             expect.messageWhenExpecting(anonymousFunc).not.isAFunction().toEqual('Expected function () {} not to be a function.');
             expect(doSomethingCool).isAFunction();
             expect(callMe).isAFunction();
+            expect.messageWhenExpecting({}).isAFunction({withName: 'foo'}).toEqual('Expected {  } to be a function.');
             expect.messageWhenExpecting(anonymousFunc).isAFunction({withName: 'foo'}).toEqual('Function () {} isn\'t named foo.');
             expect.messageWhenExpecting(doSomethingCool).not.isAFunction({withName: 'doSomethingCool'}).toEqual('Function doSomethingCool() {} is named doSomethingCool.');
             expect(callMe).isAFunction({withName: 'callMe'});
@@ -274,35 +314,47 @@
             var anonymousConstructor = (function () {});
             var Apple = (function Apple() {});
             var Banana = (function Banana() {});
-            expect(null).not.toBeAWellBehavedConstructor();
+            expect.messageWhenExpecting(null).toBeAWellBehavedConstructor().toEqual('Expected null to be a function.');
             expect({}).not.toBeAWellBehavedConstructor();
-            expect(anonymousConstructor).toBeAWellBehavedConstructor();
+            expect.messageWhenExpecting(anonymousConstructor).not.toBeAWellBehavedConstructor().toEqual(
+                'Expected anonymous function not to be a well behaved constructor with parent class Object, creating objects of class (anonymous) when called "new (function () {})()".');
+            expect.messageWhenExpecting(Apple).not.toBeAWellBehavedConstructor().toEqual(
+                'Expected Apple not to be a well behaved constructor with parent class Object, creating objects of class Apple when called "new Apple()".');
+            expect.messageWhenExpecting(Apple).not.toBeAWellBehavedConstructor({withArbitrary: 'parentClass'}).toEqual(
+                'Expected Apple not to be a well behaved constructor, creating objects of class Apple when called "new Apple()".');
+            expect.messageWhenExpecting(Apple).not.toBeAWellBehavedConstructor({withName: 'Apple'}).toEqual(
+                'Expected Apple not to be a well behaved constructor with name Apple and parent class Object, creating objects of class Apple when called "new Apple()".');
             expect(Apple).toBeAWellBehavedConstructor();
             expect(Banana).toBeAWellBehavedConstructor();
-            expect(anonymousConstructor).not.toBeAWellBehavedConstructor({withName: 'Carrot'});
+            expect.messageWhenExpecting(anonymousConstructor).toBeAWellBehavedConstructor({withName: 'Carrot'}).toEqual(
+                'Expected anonymous function to be a function with name Carrot.');
             expect(Apple).toBeAWellBehavedConstructor({withName: 'Apple'});
             expect(Banana).toBeAWellBehavedConstructor({withName: 'Banana'});
             expect(Apple).not.toBeAWellBehavedConstructor({withName: 'Banana'});
+            expect.messageWhenExpecting(Apple).toBeAWellBehavedConstructor({withName: 'Banana'}).toEqual(
+                'Expected Apple to be a function with name Banana.');
             expect(Banana).not.toBeAWellBehavedConstructor({withName: 'Apple'});
             var HasNullPrototype = (function () {});
             HasNullPrototype.prototype = null;
             var HasUndefPrototype = (function () {});
             HasUndefPrototype.prototype = undefined;
-            expect(HasNullPrototype).not.toBeAWellBehavedConstructor();
+            expect.messageWhenExpecting(HasNullPrototype).toBeAWellBehavedConstructor().toEqual('Expected anonymous function to have a non-null prototype.');
             expect(HasUndefPrototype).not.toBeAWellBehavedConstructor();
             var HasNullConstructor = (function () {});
             HasNullConstructor.prototype.constructor = null;
             var HasUndefConstructor = (function () {});
             HasUndefConstructor.prototype.constructor = undefined;
             expect(HasNullConstructor).not.toBeAWellBehavedConstructor();
-            expect(HasUndefConstructor).not.toBeAWellBehavedConstructor();
+            expect.messageWhenExpecting(HasUndefConstructor).toBeAWellBehavedConstructor().toEqual('Expected anonymous function\'s prototype to point to itself.');
             var Vehicle = (function Vehicle() {});
             var Car = (function Car() {});
             Car.prototype = Object.create(Vehicle.prototype);
             Car.prototype.constructor = Car;
-            expect(Car).not.toBeAWellBehavedConstructor();
-            expect(Car).toBeAWellBehavedConstructor({withParentClass: Vehicle});
-            expect(Car).toBeAWellBehavedConstructor({withArbitrary: 'parentClass'});
+            expect.messageWhenExpecting(Car).toBeAWellBehavedConstructor().toEqual('Expected Car\'s parent class to be Object instead of Vehicle.');
+            expect.messageWhenExpecting(Car).not.toBeAWellBehavedConstructor({withParentClass: Vehicle}).toEqual(
+                'Expected Car not to be a well behaved constructor with parent class Vehicle, creating objects of class Car when called "new Car()".');
+            expect.messageWhenExpecting(Car).not.toBeAWellBehavedConstructor({withArbitrary: 'parentClass'}).toEqual(
+                'Expected Car not to be a well behaved constructor, creating objects of class Car when called "new Car()".');
             var Boat = (function Boat() {});
             Boat.prototype = Object.create(Vehicle.prototype);
             Boat.prototype.constructor = Boat;
